fbcdc9823b8792b1df6369d3440bca0075608d8e
Alice Freda <alicefreda@gmail.com>
2019-11-11 00:19:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ class Song
     @@all << self
   end
   def self.new_by_filename(filename)
-    new_song = Song.new(filename.split(" - ")[1])
+    new_song = self.new(filename.split(" - ")[1])
     new_song.artist_name=(filename.split(" - ")[0])
     new_song
   end
